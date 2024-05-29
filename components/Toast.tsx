d4a2912122f9470dc6b05886f0395e2d371d414a
suntoes <mar.suntoes@gmail.com>
2024-05-29 12:08:32 +0800

    toast

--- components/Toast.tsx
+++ components/Toast.tsx
@@ -3,21 +3,24 @@
 import { useToast } from '@chakra-ui/react'
 import { useCookies } from 'react-cookie'
 import { cookeys } from 'lib/resources'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 export const Toast = () => {
     const [cookies, setCookie, removeCookie] = useCookies()
     const toast = useToast()
 
+    const toastRef = useRef('')
+
     useEffect(() => {
         const toastCookie = cookies[cookeys.TOAST]
-        if (toastCookie?.description) {
+        if (toastCookie?.description && toastRef.current !== toastCookie?.description) {
             toast({
                 title: toastCookie?.title,
                 description: toastCookie?.description,
                 status: toastCookie?.status,
                 isClosable: true,
             })
+            toastRef.current = toastCookie?.description
             removeCookie(cookeys.TOAST)
         }
     }, [cookies, removeCookie])
